c006bc4b44992f046a2a1e423d06abacaad33af9
allen <893102715@qq.com>
2019-03-22 13:30:58 +0800

    update

--- scrapy.py
+++ scrapy.py
@@ -67,6 +67,7 @@ html = soup.prettify()
 
 
 soup = BeautifulSoup(open('baidu.html','r',encoding='utf-8'),'lxml')
+
 # print(type(soup))
 # 四大对象
 #   TAG：通俗点讲就是html中的标签对于Tag有两个重要的属性：
@@ -76,9 +77,29 @@ soup = BeautifulSoup(open('baidu.html','r',encoding='utf-8'),'lxml')
 #           是一个特殊的Tag，我们可以分别获取它的类型，名称以及属性
 #   Comment:Comment对象是一个特殊类型的NavigableString对象，其输出内容不包括注释符号
 
-#  Tag
-print(soup.html.head.meta)    # 通过标签获取
-# 标签的属性：Name Attrs
-print(soup.div.input.name)    #返回标签姓名
-print(soup.div.input.attrs)    #返回标签里的属性，字典的格式   字典索引：soup.div.input.attrs['type']
+#  Tag   拿标签
+# print(soup.html.head.meta)    # 通过标签获取
+# # 标签的属性：Name Attrs
+# print(soup.div.input.name)    #返回标签姓名
+# print(soup.div.input.attrs)    #返回标签里的属性，字典的格式   字典索引：soup.div.input.attrs['type']
+# #   NavigableString　　　　　拿标签的文本内容
+# print(soup.html.title.string)    #即使注释掉也可以拿到,注释掉了就是comment,没注释就是 NavigableString
+# print(type(soup.html.title.string))
+
+# 如何遍历文档树？
+    # 直接子节点：
+        # contents:标签的contents属性可以将Tag的子节点以列表的方式输出
+        # children:返回一个可迭代的对象
+# print(soup.head.contents)    #空格，换行都会算作子节点加入列表中显示，需要手动计算需要的标签是第几个子节点
+# print(soup.head.contents[7])   #列表索引提取子节点
+# print(soup.head.contents[7].attrs['href'])   #列表索引提取子节点的标签属性
+    # 所有子孙节点：
+        # descendants属性可以对所有的Tag的子孙节点进行递归循环，和children类似，需要遍历获取内容
+print(soup.head.children)   #返回一个迭代对象
+for d in soup.head.children:
+    print(d)
+    # 节点内容：
+        # string:返回标签里面的内容
+        # text:返回标签的文本
+
 
